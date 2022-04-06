c3bf6fff7461216347652c34275d2154f8809258
Anurag Sahu <54500608+AnuragSahu11@users.noreply.github.com>
2022-04-07 00:20:21 +0530

    [feat]Added more videos to database

--- TradePlay/src/backend/db/videos.js
+++ TradePlay/src/backend/db/videos.js
@@ -63,24 +63,133 @@ export const videos = [
     views: 300,
     category: "crypto",
   },
-  // {
-  //   _id: "mindset_1",
-  //   title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-  //   creator: "Sentdex",
-  //   videoLink: "",
-  //   description:
-  //     "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-  //   views: 300,
-  //   category: "stock",
-  // },
-  // {
-  //   _id: "mindset_1",
-  //   title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-  //   creator: "Sentdex",
-  //   videoLink: "",
-  //   description:
-  //     "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-  //   views: 300,
-  //   category: "stock",
-  // },
+  {
+    _id: "g1nytORNq0E",
+    title: "Stock Market Terminology Every Trader Must Know",
+    creator: "Rayner Teo",
+    videoLink:
+      "https://www.youtube.com/embed/g1nytORNq0E?list=PLT6_Bt_TKitIUCEsI-3SGCrVHCy0g_5jd",
+    description:
+      "In this lesson, you'll discover the essential stock market terminology that every trader must know.",
+    views: 300,
+    category: "stock",
+  },
+  {
+    _id: "QYjB43EqraQ",
+    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
+    creator: "Sentdex",
+    videoLink:
+      "https://www.youtube.com/embed/QYjB43EqraQ?list=PLT6_Bt_TKitIUCEsI-3SGCrVHCy0g_5jd",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    views: 300,
+    category: "stock",
+  },
+  {
+    _id: "U9nsjYNV5yo",
+    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
+    creator: "Sentdex",
+    videoLink:
+      "https://www.youtube.com/embed/U9nsjYNV5yo?list=PLT6_Bt_TKitIUCEsI-3SGCrVHCy0g_5jd",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    views: 300,
+    category: "stock",
+  },
+  {
+    _id: "X2ptmvfKp6Q",
+    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
+    creator: "Sentdex",
+    videoLink:
+      "https://www.youtube.com/embed/X2ptmvfKp6Q?list=PLT6_Bt_TKitIUCEsI-3SGCrVHCy0g_5jd",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    views: 300,
+    category: "stock",
+  },
+  {
+    _id: "GuZkcooBBro",
+    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
+    creator: "Sentdex",
+    videoLink:
+      "https://www.youtube.com/embed/GuZkcooBBro?list=PLT6_Bt_TKitIUCEsI-3SGCrVHCy0g_5jd",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    views: 300,
+    category: "stock",
+  },
+  {
+    _id: "L1MTCOi2zZE",
+    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
+    creator: "Sentdex",
+    videoLink:
+      "https://www.youtube.com/embed/L1MTCOi2zZE?list=PLT6_Bt_TKitIUCEsI-3SGCrVHCy0g_5jd",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    views: 300,
+    category: "stock",
+  },
+  {
+    _id: "zRW8nL1Sjtc",
+    title: "Traders Mindset? || Psychology Series",
+    creator: "Sentdex",
+    videoLink: "https://www.youtube.com/embed/zRW8nL1Sjtc",
+    description:
+      "Booming Bulls is an initiative for the young generation who is interested in making more,willing to step forward and be free from financial situations. We provide knowledge that is based on self-learning, experience and theoretical implications on the market.",
+    views: 300,
+    category: "trading_mindset",
+  },
+  {
+    _id: "vtmvk5m7wfI",
+    title:
+      "How to Master Your Trading Psychology (Strategies That Actually Work)",
+    creator: "Sentdex",
+    videoLink: "https://www.youtube.com/embed/vtmvk5m7wfI",
+    description:
+      "Discover how you can manage your trading psychology with practical strategies that work (without meditation or hypnosis).",
+    views: 300,
+    category: "trading_mindset",
+  },
+  {
+    _id: "iGcJlDu-WbE",
+    title:
+      "Psychological Trading Mistakes (6 Ways Your Mind Is Tricking You Into Being a Losing Trader)",
+    creator: "Sentdex",
+    videoLink: "https://www.youtube.com/embed/iGcJlDu-WbE",
+    description:
+      "The most successful stock and forex traders are the ones who have developed an edge. Before you dive into market analysis and dream at profitable stock trading techniques and strategies, you must work on your trading mindset.",
+    views: 300,
+    category: "trading_mindset",
+  },
+  {
+    _id: "E65ruK-_Mrc",
+    title: "Hope/Fear/Greed - Managing Emotions in Trading",
+    creator: "Sentdex",
+    videoLink: "https://www.youtube.com/embed/E65ruK-_Mrc",
+    description:
+      "Hope/Fear/Greed - Managing Emotions in Trading  David Paul, Financial Trader and Managing Director of Vector Vest UK comments.Give demo accounts a month or two but then switch to a real money account - virtual accounts lack the emotions - so use a real money account but start very small.",
+    views: 300,
+    category: "trading_mindset",
+  },
+  {
+    _id: "dyV1NYbk_nA",
+    title:
+      "Top 3 Trading Psychology Lessons I Leaned In 10 Years (no emotions = no mistakes)",
+    creator: "Sentdex",
+    videoLink: "https://www.youtube.com/embed/dyV1NYbk_nA",
+    description:
+      "In this video Steven will be teaching you about the most important part of your trading. What is it? Trading psychology. That's right. It is not a strategy, or which indicator you use. It is not the latest structure level or entry reason. Trading psychology is will make or break you in this industry. So, let's take a look at how you can master your emotions and take control of your trading psychology.",
+    views: 300,
+    category: "trading_mindset",
+  },
+  {
+    _id: "",
+    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
+    creator: "Sentdex",
+    videoLink: "https://www.youtube.com/embed/",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    views: 300,
+    category: "trading_mindset",
+  },
 ];
