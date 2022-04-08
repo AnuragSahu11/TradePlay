ca47356531eb0fe709468359632cce2927de4ba7
Anurag Sahu <54500608+AnuragSahu11@users.noreply.github.com>
2022-04-08 18:39:44 +0530

    [feat]Added barrel imports in utility folder

--- TradePlay/src/utils/index.jsx
+++ TradePlay/src/utils/index.jsx
@@ -1,1 +1,5 @@
 export { changeTitle } from "./change-title";
+export { addToHistory } from "./add-to-history";
+export { AddToWatchLaterSmall } from "./add-to-watchlater-small";
+export { getApiData } from "./get-api-data";
+export { inList } from "./in-list";
