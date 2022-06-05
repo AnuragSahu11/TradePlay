adeb64eefb958294f489b835f0998c3ed4d023cf
Anurag Sahu <54500608+AnuragSahu11@users.noreply.github.com>
2022-06-05 20:09:25 +0530

    [fix]Fixed token issue on login

--- TradePlay/src/context/auth-context/auth-context.jsx
+++ TradePlay/src/context/auth-context/auth-context.jsx
@@ -12,16 +12,14 @@ const useAuth = () => useContext(AuthContext);
 
 const AuthProvider = ({ children }) => {
   const initialUserData = {
-    token: "",
+    token: localStorage.getItem("token") || null,
     history: [],
     playlists: [],
     likes: [],
     watchlater: [],
   };
 
-  useEffect(() => {
-    
-  });
+  useEffect(() => {});
 
   const [userDataState, userDataDispatch] = useReducer(
     userDataReducer,
